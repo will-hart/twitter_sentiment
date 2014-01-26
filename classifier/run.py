6c1f5dc57d11edb30c2e0fcd9b9aa77c77a68f12
will-hart <hart.wl@gmail.com>
2014-01-26 14:05:49 +0000

    Update running code

--- classifier/run.py
+++ classifier/run.py
@@ -21,6 +21,14 @@ t = TC("train_data.txt")
 t.train()
 
 # test the classifier
+tested = 0
+correct = 0
+
 with open('test_data.txt', 'r') as f:
     for line in f.readlines():
-        run_test(line[:-1], line[-1])
+        tested += 1
+        line = line[:-1]
+        if t.predict(line[:-1]) == int(line[-1]):
+            correct += 1
+
+print "Tested {0} tweets, got {1} correct ({2:.0%})".format(tested, correct, correct/tested)
